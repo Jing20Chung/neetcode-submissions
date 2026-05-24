3b4d85a4e05bf7fd43a47cf5d8f3b453a9434590
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-05-24 11:15:28 -0400

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.cpp
@@ -0,0 +1,63 @@
+class Twitter {
+public:
+    Twitter() {
+        time = 0;
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        userTweet[userId].push_back({time++, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        auto compare = [](vector<int>& a, vector<int>& b){
+            return a[0] < b[0];
+        };
+
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> minHeap(compare);
+
+        followMap[userId].insert(userId);
+        
+        for(int followeeId: followMap[userId]) {
+            if(userTweet.count(followeeId)) {
+                vector<vector<int>>& tweet = userTweet[followeeId];
+                int index = tweet.size() - 1;
+                minHeap.push({tweet[index][0], tweet[index][1], followeeId, index});
+            }
+        }
+
+        vector<int> res;
+        while(!minHeap.empty() && res.size() < 10) {
+            vector<int> curr = minHeap.top();
+            minHeap.pop();
+
+            int time = curr[0];
+            int tweetId = curr[1];
+            int followeeId = curr[2];
+            int index = curr[3];
+
+            res.push_back(tweetId);
+
+            if (index > 0) {
+                const vector<int>& tweet = userTweet[followeeId][index - 1];
+                minHeap.push({tweet[0], tweet[1], followeeId, index - 1});
+            }
+        }
+
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        followMap[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        followMap[followerId].erase(followeeId);
+    }
+
+
+private:
+    int time;
+    unordered_map<int, vector<vector<int>>> userTweet;
+    unordered_map<int, set<int>> followMap;
+
+};
