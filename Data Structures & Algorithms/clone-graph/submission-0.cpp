f7f7ad3c5d8bc4be2e05be475e7e1d958f6fc9b4
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-05-28 10:33:47 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        unordered_map<Node*, Node*> nodeMap;
+        nodeMap[node] = new Node(node->val);
+        queue<Node*> q;
+        q.push(node);
+
+        while(!q.empty()) {
+            Node* cur = q.front();
+            q.pop();
+            for(Node* neighbor: cur->neighbors) {
+                if(nodeMap.find(neighbor) == nodeMap.end()) {
+                    nodeMap[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                nodeMap[cur]->neighbors.push_back(nodeMap[neighbor]);
+            }
+        }
+        return nodeMap[node];
+    }
+};
