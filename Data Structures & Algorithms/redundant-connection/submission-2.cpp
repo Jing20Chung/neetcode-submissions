28087425c8b45c3d0a6264b2baf88d42032c669c
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-12 11:27:25 -0400

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        if (edges.size() == 0) return {};
+
+        int n = edges.size();
+        vector<int> parent(n + 1, 0);
+        // init parent to itself
+        for(int i = 1; i <= n; ++i) {
+            parent[i] = i;
+        }
+
+        for(const vector<int>& edge: edges) {
+            int root1 = find(edge[0], parent);
+            int root2 = find(edge[1], parent);
+            if (root1 != root2) {
+                parent[root1] = root2;
+            }
+            else {
+                return {edge[0], edge[1]};
+            }
+        }
+        return {};
+    }
+
+    int find(int node, vector<int>& parent) {
+        if (parent[node] == node) {
+            return node;
+        }
+        return parent[node] = find(parent[node], parent);
+    }
+};
