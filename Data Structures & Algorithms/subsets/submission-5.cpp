cd89f04af3d2837f4098b84b095250dd4e227023
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-13 14:38:09 -0400

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    void buildSubsets(vector<vector<int>>& ans, int i, vector<int>& nums, vector<int>& curr) {
+        if (i >= nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        buildSubsets(ans, i + 1, nums, curr);
+        curr.pop_back();
+        buildSubsets(ans, i + 1, nums, curr);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        buildSubsets(ans, 0, nums, curr);
+        return ans;        
+    }
+};
