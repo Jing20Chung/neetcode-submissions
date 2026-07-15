f087f728d7b2afd51b0c773ab025bf803bf4ea2e
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-15 16:24:55 -0400

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<int, int> dp;
+        dp[0] = 1;
+        for(int i = 0; i < nums.size(); ++i) {
+            unordered_map<int, int> nextDp;
+            for(auto[sum, count]: dp) {
+                nextDp[sum + nums[i]] += count;
+                nextDp[sum - nums[i]] += count;
+            }
+            dp = nextDp;
+        }
+
+        return dp[target];
+    }
+};
