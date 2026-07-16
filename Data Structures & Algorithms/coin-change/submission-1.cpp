40fc6bd779a11adbe4e498dbcb6845f7805e1b91
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-16 08:56:21 -0400

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for(int a = 1; a <= amount; ++a) {
+            for(int coin: coins) {
+                if ((a - coin) >= 0) {
+                    dp[a] = min(dp[a - coin] + 1, dp[a]);
+                }
+            }
+        }
+
+        return dp[amount] == amount + 1? -1: dp[amount];
+    }
+};
