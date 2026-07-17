0e45487bee96f87e50592dec73faf75dd47eb6cf
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-17 13:37:28 -0400

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int prev = 0, prev2 = 0;
+        for(int i = 2; i <= cost.size(); ++i) {
+            int tmp = min(prev + cost[i - 1] , prev2 + cost[i - 2]);
+            prev2 = prev;
+            prev = tmp;
+        }
+        return prev;
+    }
+};
