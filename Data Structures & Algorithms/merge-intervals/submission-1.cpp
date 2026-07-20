89af662165deeb2f801281e9619fc49a53020ecd
Jing Chung <154283751+Jing20Chung@users.noreply.github.com>
2026-07-20 11:03:13 -0400

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
+        vector<vector<int>> res;
+        for(auto& interval: intervals) {
+            if (res.empty() || res.back()[1] < interval[0]) {
+                res.push_back({interval[0], interval[1]});
+            }
+            else {
+                res.back()[1] = max(res.back()[1], interval[1]);
+            }
+        }
+        return res;
+    }
+};
